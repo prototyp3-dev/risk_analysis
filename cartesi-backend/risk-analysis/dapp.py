8dae6a589e59f3d83dbd896b862ea4cd3890d5cc
Arthur <arthur@DESKTOP-C88PO09>
2024-06-14 16:56:08 -0300

    model test inside Cartesi Machine

--- cartesi-backend/risk-analysis/dapp.py
+++ cartesi-backend/risk-analysis/dapp.py
@@ -1,10 +1,12 @@
 import logging
 
 from cartesi import DApp, Rollup, RollupData
+import pickle
 
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 dapp = DApp()
+model = pickle.load(open("model/model.pkl", "rb"))
 
 
 def str2hex(str):
@@ -22,11 +24,26 @@ def handle_advance(rollup: Rollup, data: RollupData) -> bool:
 
 @dapp.inspect()
 def handle_inspect(rollup: Rollup, data: RollupData) -> bool:
-    payload = data.str_payload()
-    LOGGER.debug("Echoing '%s'", payload)
-    rollup.report(str2hex(payload))
-    return True
+    try:
+        # [UF, IDADE, ESCOLARIDADE, ESTADO_CIVIL, QT_FILHOS, CASA_PROPRIA, QT_IMOVEIS, VL_IMOVEIS, OUTRA_RENDA,
+        # OUTRA_RENDA_VALOR, TEMPO_ULTIMO_EMPREGO_MESES, TRABALHANDO_ATUALMENTE, ULTIMO_SALARIO, QT_CARROS,
+        # VALOR_TABELA_CARROS, FAIXA_ETARIA]
 
+        # normalized data
+        dados = [[
+            0.5, 0.5, 0.5, 0.66666667, 0.33333333, 1.0,
+            0.33333333, 0.33333333, 1.0, 0.5, 0.07042254,
+            1.0, 0.17821782, 2.0, 0.38888889, 0.75
+        ]]
+
+        payload = str(model.predict(dados))
+        rollup.report(str2hex(payload))
+
+        return True
+
+    except Exception as e:
+        rollup.report(str2hex(str(e)))
+        return False
 
 if __name__ == '__main__':
     dapp.run()
